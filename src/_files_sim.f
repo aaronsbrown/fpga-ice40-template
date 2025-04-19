41f0ed408ed9b74b1b6a6250b6723bb13b121328
aaronsbrown <aaron.s.brown@gmail.com>
2025-04-19 16:35:34 -0700

    first commit

---
+++ src/_files_sim.f
@@ -0,0 +1,6 @@
+# List source files for simulation, relative to repo root
+src/utils/timescale.v     # Optional timescale definition
+src/core/arch_defs_pkg.sv # Optional placeholder package
+src/top.sv
+test/test_utilities_pkg.sv # Must include test utilities
+# Add your design files here (same as synth list usually)
---
+++ src/_files_synth.f
@@ -0,0 +1,4 @@
+# List source files for synthesis, relative to repo root
+src/core/arch_defs_pkg.sv 
+src/top.sv
+# Add your design files here
